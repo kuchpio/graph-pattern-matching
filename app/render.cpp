1f5fcd193f00b6eb9dc9c0136c67bb54c7b55b2b
kuchpio <53230073+kuchpio@users.noreply.github.com>
2024-10-21 22:12:45 +0200

    hello graph

--- app/render.cpp
+++ app/render.cpp
@@ -1,65 +1,98 @@
 #include <iostream>
+#include <vector>
+
 #define GLAD_GL_IMPLEMENTATION
 #include "glad/gl.h"
 #include "GLFW/glfw3.h"
-#include "linmath.h"
 
 #include "render.h"
 
-static const struct {
-    float x, y;
-    float r, g, b;
-} vertices[3] = {
-    {-0.6f, -0.4f, 1.f, 0.f, 0.f}, 
-    {0.6f, -0.4f, 0.f, 1.f, 0.f}, 
-    {0.f, 0.6f, 0.f, 0.f, 1.f}
-};
+GraphRenderer::GraphRenderer() : _vertexCount(0), _edgesCount(0) {
+    glGenVertexArrays(1, &_vertexArrayObject);
+    glGenBuffers(1, &_vertexBuffer);
+    glGenBuffers(1, &_edgesBuffer);
 
-static const char* vertex_shader_text = 
-    "#version 110\n"
-	"uniform mat4 MVP;\n"
-	"attribute vec3 vCol;\n"
-	"attribute vec2 vPos;\n"
-	"varying vec3 color;\n"
-	"void main()\n"
-	"{\n"
-	"    gl_Position = MVP * vec4(vPos, 0.0, 1.0);\n"
-	"    color = vCol;\n"
-	"}\n";
+    glPointSize(VERTEX_SIZE);
+    glLineWidth(EDGE_WIDTH);
+}
 
-static const char* fragment_shader_text = 
-    "#version 110\n"
-	"varying vec3 color;\n"
-	"void main()\n"
-	"{\n"
-	"    gl_FragColor = vec4(color, 1.0);\n"
-	"}\n";
+void GraphRenderer::setVertexPositions(const float* positions2D, unsigned int vertexCount) {
+    glBindVertexArray(_vertexArrayObject);
+    glBindBuffer(GL_ARRAY_BUFFER, _vertexBuffer);
+    glBufferData(GL_ARRAY_BUFFER, sizeof(float) * vertexCount * 2, positions2D, GL_STATIC_DRAW);
+
+    glVertexAttribPointer(0, 2, GL_FLOAT, GL_FALSE, 0, (void*)0);
+    glEnableVertexAttribArray(0);
+
+    _vertexCount = vertexCount;
+}
+
+void GraphRenderer::setEdges(const core::Graph& graph) {
+    std::vector<unsigned int> edges;
+    
+    // TODO: Get edges info from graph
+    for (int i = 0; i < graph.size() - 1; i++) {
+        edges.push_back(i);
+        edges.push_back(i + 1);
+    }
+
+    edges.push_back(graph.size() - 1);
+    edges.push_back(0);
+
+    glBindVertexArray(_vertexArrayObject);
+    glBindBuffer(GL_ELEMENT_ARRAY_BUFFER, _edgesBuffer);
+    glBufferData(GL_ELEMENT_ARRAY_BUFFER, sizeof(unsigned int) * edges.size(), edges.data(), GL_STATIC_DRAW);
+
+    _edgesCount = edges.size();
+}
+
+void GraphRenderer::render(int width, int height) const {
+	glViewport(0, 0, width, height);
+	glClear(GL_COLOR_BUFFER_BIT);
+
+	glBindVertexArray(_vertexArrayObject);
+
+	glColor3f(1.0f, 1.0f, 1.0f);
+	glDrawElements(GL_LINES, _edgesCount, GL_UNSIGNED_INT, 0);
+
+	glColor3f(1.0f, 0.0f, 0.0f);
+	glDrawArrays(GL_POINTS, 0, _vertexCount);
+
+	glBindVertexArray(0);
+}
+
+GraphRenderer::~GraphRenderer() {
+    glDeleteBuffers(1, &_edgesBuffer);
+    glDeleteBuffers(1, &_vertexBuffer);
+    glDeleteBuffers(1, &_vertexArrayObject);
+}
 
 static void error_callback(int error, const char* description) {
     fprintf(stderr, "Error: %s\n", description);
 }
 
 static void key_callback(GLFWwindow* window, int key, int scancode, int action, int mods) {
-    if (key == GLFW_KEY_ESCAPE && action == GLFW_PRESS) 
-        glfwSetWindowShouldClose(window, GLFW_TRUE);
+    if (key == GLFW_KEY_ESCAPE && action == GLFW_PRESS) glfwSetWindowShouldClose(window, GLFW_TRUE);
 }
 
 void testRender() {
-    GLFWwindow* window;
-    GLuint vertex_buffer, vertex_shader, fragment_shader, program;
-    GLint mvp_location, vpos_location, vcol_location;
+    const float vertices[] = {
+        -0.5f, 0.3f, 
+        0.4f, 0.1f, 
+        -0.7f, 0.9f, 
+        0.0f, 0.3f, 
+        0.2f, -0.5f
+    };
+    auto graph = core::Graph(5);
 
     glfwSetErrorCallback(error_callback);
 
-    if (!glfwInit()) exit(EXIT_FAILURE);
+    if (!glfwInit()) return;
 
-    glfwWindowHint(GLFW_CONTEXT_VERSION_MAJOR, 2);
-    glfwWindowHint(GLFW_CONTEXT_VERSION_MINOR, 0);
-
-    window = glfwCreateWindow(640, 480, "Hello GLFW!", NULL, NULL);
+    auto window = glfwCreateWindow(640, 480, "Hello GLFW!", NULL, NULL);
     if (!window) {
         glfwTerminate();
-        exit(EXIT_FAILURE);
+        return;
     }
 
     glfwSetKeyCallback(window, key_callback);
@@ -68,57 +101,21 @@ void testRender() {
     gladLoadGL(glfwGetProcAddress);
     glfwSwapInterval(1);
 
-    // NOTE: OpenGL error checks have been omitted for brevity
-
-    glGenBuffers(1, &vertex_buffer);
-    glBindBuffer(GL_ARRAY_BUFFER, vertex_buffer);
-    glBufferData(GL_ARRAY_BUFFER, sizeof(vertices), vertices, GL_STATIC_DRAW);
-
-    vertex_shader = glCreateShader(GL_VERTEX_SHADER);
-    glShaderSource(vertex_shader, 1, &vertex_shader_text, NULL);
-    glCompileShader(vertex_shader);
-
-    fragment_shader = glCreateShader(GL_FRAGMENT_SHADER);
-    glShaderSource(fragment_shader, 1, &fragment_shader_text, NULL);
-    glCompileShader(fragment_shader);
-
-    program = glCreateProgram();
-    glAttachShader(program, vertex_shader);
-    glAttachShader(program, fragment_shader);
-    glLinkProgram(program);
-
-    mvp_location = glGetUniformLocation(program, "MVP");
-    vpos_location = glGetAttribLocation(program, "vPos");
-    vcol_location = glGetAttribLocation(program, "vCol");
-
-    glEnableVertexAttribArray(vpos_location);
-    glVertexAttribPointer(vpos_location, 2, GL_FLOAT, GL_FALSE, sizeof(vertices[0]), (void*)0);
-    glEnableVertexAttribArray(vcol_location);
-    glVertexAttribPointer(vcol_location, 3, GL_FLOAT, GL_FALSE, sizeof(vertices[0]), (void*)(sizeof(float) * 2));
+    GraphRenderer graphRenderer;
+    graphRenderer.setEdges(graph);
+    graphRenderer.setVertexPositions(vertices, graph.size());
 
     while (!glfwWindowShouldClose(window)) {
-        float ratio;
         int width, height;
-        mat4x4 m, p, mvp;
 
         glfwGetFramebufferSize(window, &width, &height);
-        ratio = width / (float)height;
 
-        glViewport(0, 0, width, height);
-        glClear(GL_COLOR_BUFFER_BIT);
-
-        mat4x4_identity(m);
-        mat4x4_rotate_Z(m, m, (float)glfwGetTime());
-        mat4x4_ortho(p, -ratio, ratio, -1.f, 1.f, 1.f, -1.f);
-        mat4x4_mul(mvp, p, m);
-
-        glUseProgram(program);
-        glUniformMatrix4fv(mvp_location, 1, GL_FALSE, (const GLfloat*)mvp);
-        glDrawArrays(GL_TRIANGLES, 0, 3);
+        graphRenderer.render(width, height);
 
         glfwSwapBuffers(window);
         glfwPollEvents();
     }
+    graphRenderer.~GraphRenderer();
 
     glfwDestroyWindow(window);
 
