693faafff196647b1006f764dbabc59452fc5b06
Willu12 <boryskurdek@gmail.com>
2024-10-29 09:51:38 +0100

    fixed induced minor algorithms

--- pattern/src/induced_minor_matcher.cpp
+++ pattern/src/induced_minor_matcher.cpp
@@ -14,18 +14,23 @@ bool InducedMinorMatcher::minor_induced_recursion(const core::Graph& G, const co
                                                   std::optional<int> last_neighbour_index) {
     if (H.size() > G.size()) return false;
 
-    auto isomorphismMatcher = IsomorphismMatcher();
-    if (isomorphismMatcher.match(G, H)) return true;
+    if (this->isomorphismMatcher.match(G, H)) return true;
 
-    if (v > G.size()) return false;
+    if (v >= G.size()) return false;
 
     int start_neighbour_index = last_neighbour_index.value_or(0);
-    if (minor_induced_recursion(remove_vertex(G, v), H, v, {})) return true;
+
+    if (G.size() > H.size()) {
+        auto G_after_removal = remove_vertex(G, v);
+        if (minor_induced_recursion(G_after_removal, H, v + 1, std::nullopt)) return true;
+    }
+
     for (int neighbour_index = start_neighbour_index; neighbour_index < G.get_neighbours(v).size(); neighbour_index++) {
         auto neighbour = G.get_neighbours(v)[neighbour_index];
-        if (minor_induced_recursion(remove_edge(G, v, neighbour), H, v, neighbour_index)) return true;
-        if (minor_induced_recursion(extract_edge(G, v, neighbour), H, v, neighbour_index)) return true;
+        auto G_after_edge_extraction = extract_edge(G, v, neighbour);
+        if (minor_induced_recursion(G_after_edge_extraction, H, v, neighbour_index)) return true;
     }
-    return minor_induced_recursion(G, H, v + 1, {});
+
+    return minor_induced_recursion(G, H, v + 1, std::nullopt);
 }
 } // namespace pattern
--- pattern/src/topological_induced_minor_matcher.cpp
+++ pattern/src/topological_induced_minor_matcher.cpp
@@ -12,22 +12,25 @@ bool TopologicalInducedMinorMatcher::match(const core::Graph& G, const core::Gra
 bool TopologicalInducedMinorMatcher::topological_induced_minor_recursion(const core::Graph& G, const core::Graph& H,
                                                                          int v,
                                                                          std::optional<int> last_neighbour_index) {
-
     if (H.size() > G.size()) return false;
 
-    auto isomorphismMatcher = IsomorphismMatcher();
-    if (isomorphismMatcher.match(G, H)) return true;
+    if (this->isomorphismMatcher.match(G, H)) return true;
 
-    if (v > G.size()) return false;
+    if (v >= G.size()) return false;
 
     int start_neighbour_index = last_neighbour_index.value_or(0);
-    if (topological_induced_minor_recursion(remove_vertex(G, v), H, v, {})) return true;
 
-    if (G.degree_in(v) == 1 && G.degree_out(v) == 1) {
-        if (topological_induced_minor_recursion(extract_edge(G, v, G.get_neighbours(v)[0]), H, v, {})) return true;
+    if (G.size() > H.size()) {
+        auto G_after_removal = remove_vertex(G, v);
+        if (topological_induced_minor_recursion(G_after_removal, H, v + 1, std::nullopt)) return true;
     }
 
-    return topological_induced_minor_recursion(G, H, v + 1, {});
+    if (G.degree_in(v) == 1 && G.degree_out(v) == 1) {
+        auto G_after_edge_extraction = extract_edge(G, v, G.get_neighbours(v)[0]);
+        if (topological_induced_minor_recursion(G_after_edge_extraction, H, v, std::nullopt)) return true;
+    }
+
+    return topological_induced_minor_recursion(G, H, v + 1, std::nullopt);
 }
 
 } // namespace pattern
