f4751b9efd2710acb408f374a2c5407d89949d99
Willu12 <boryskurdek@gmail.com>
2024-11-17 14:17:31 +0100

    added vf2lib isomorphism solver

---
+++ pattern/include/vf2libSolvers/vf2_isomorphism_solver.hpp
@@ -0,0 +1,18 @@
+#pragma once
+
+#include "core.h"
+#include "pattern.h"
+#include "argraph.h"
+#include <optional>
+
+namespace pattern
+{
+class Vf2IsomorphismSolver : public PatternMatcher {
+  public:
+    bool match(const core::Graph& bigGraph, const core::Graph& smallGraph);
+    std::optional<std::vector<vertex>> matching(const core::Graph& bigGraph, const core::Graph& smallGraph);
+
+  private:
+    Graph convert_graph(const core::Graph& G);
+};
+} // namespace pattern
---
+++ pattern/src/vf2libSolvers/vf2_isomorphism_solver.cpp
@@ -0,0 +1,55 @@
+#include "vf2_state.h"
+#include "vf2_isomorphism_solver.hpp"
+
+#include "argedit.h"
+#include "argraph.h"
+#include "core.h"
+#include <vector>
+
+#include "match.h"
+
+Graph pattern::Vf2IsomorphismSolver::convert_graph(const core::Graph& G) {
+    ARGEdit ed;
+
+    for (vertex v = 0; v < G.size(); v++) {
+        ed.InsertNode(&v);
+    }
+
+    for (auto [u, v] : G.edges()) {
+        ed.InsertEdge(u, v, nullptr);
+    }
+
+    return Graph(&ed);
+}
+
+bool pattern::Vf2IsomorphismSolver::match(const core::Graph& bigGraph, const core::Graph& smallGraph) {
+    auto G = convert_graph(bigGraph);
+    auto Q = convert_graph(smallGraph);
+
+    int n;
+    VF2State s0(&Q, &G);
+
+    std::vector<node_id> big_nodes(smallGraph.size()), small_nodes(smallGraph.size());
+
+    return vf2::match(&s0, &n, big_nodes.data(), small_nodes.data());
+}
+
+std::optional<std::vector<vertex>> pattern::Vf2IsomorphismSolver::matching(const core::Graph& bigGraph,
+                                                                           const core::Graph& smallGraph) {
+    auto G = convert_graph(bigGraph);
+    auto Q = convert_graph(smallGraph);
+
+    int n;
+    VF2State s0(&Q, &G);
+
+    std::vector<node_id> big_nodes(smallGraph.size()), small_nodes(smallGraph.size());
+
+    if (vf2::match(&s0, &n, big_nodes.data(), small_nodes.data())) {
+
+        std::vector<vertex> result = std::vector<vertex>(smallGraph.size());
+        for (int i = 0; i < result.size(); i++)
+            result[i] = big_nodes[i];
+        return result;
+    }
+    return std::nullopt;
+}
