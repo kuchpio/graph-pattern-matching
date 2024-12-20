3d5893001703339bd4946ee8cadaf4d465b915d7
Willu12 <boryskurdek@gmail.com>
2024-12-20 19:54:30 +0100

    added correct return result to topological minors

--- pattern/src/minorSolvers/topological_minor_heuristic.cpp
+++ pattern/src/minorSolvers/topological_minor_heuristic.cpp
@@ -1,4 +1,5 @@
 #include "topological_minor_heuristic.h"
+#include <numeric>
 
 #define MAX_RECURSION_DEPTH 1000
 
@@ -6,10 +7,14 @@ namespace pattern
 {
 std::optional<std::vector<vertex>> TopologicalMinorHeuristic::match(const core::Graph& G, const core::Graph& H) {
     if (H.size() > G.size()) return std::nullopt;
-    return tpRecursion(G, H, 0);
+    std::vector<vertex> mapping(G.size());
+    std::iota(mapping.begin(), mapping.end(), 0);
+
+    return tpRecursion(G, H, mapping, 0);
 }
 
 std::optional<std::vector<vertex>> TopologicalMinorHeuristic::tpRecursion(const core::Graph G, const core::Graph& H,
+                                                                          const std::vector<vertex> mapping,
                                                                           int depth) {
     if (depth > MAX_RECURSION_DEPTH) return std::nullopt;
     if (H.size() > G.size()) return std::nullopt;
@@ -17,11 +22,11 @@ std::optional<std::vector<vertex>> TopologicalMinorHeuristic::tpRecursion(const 
     auto subgraphMatching = subgraphMatcher_->match(G, H);
     if (subgraphMatching) return subgraphMatching;
 
-    // iterate throug all edges
     for (auto [u, v] : G.edges()) {
-        if(G.degree_in(v) + G.degree_out(v) == 2) {
+        if (G.degree_in(v) + G.degree_out(v) == 2) {
             auto newMinor = contractEdge(G, u, v);
-            auto matching = tpRecursion(G, newMinor, depth + 1);
+            auto newMapping = updateMapping(mapping, u, v);
+            auto matching = tpRecursion(G, newMinor, newMapping, depth + 1);
             if (matching) return matching;
         }
     }
@@ -32,5 +37,25 @@ core::Graph TopologicalMinorHeuristic::contractEdge(const core::Graph& G, vertex
     core::Graph Q = core::Graph(G);
     Q.contract_edge(u, v);
     return Q;
+}
+
+std::vector<vertex> TopologicalMinorHeuristic::updateMapping(const std::vector<vertex>& mapping, vertex u, vertex v) {
+    auto newMapping = std::vector<vertex>(mapping);
+    for (vertex& vertex : newMapping) {
+        if (vertex == v)
+            vertex = u;
+        else if (vertex > v)
+            vertex--;
+    }
+    return newMapping;
+}
+
+std::vector<vertex> TopologicalMinorHeuristic::getResult(const std::vector<vertex>& mapping,
+                                                         const std::vector<vertex>& contractedResult) {
+    auto result = std::vector<vertex>(mapping);
+    for (vertex& v : result) {
+        v = contractedResult[v];
+    }
+    return result;
 }
 } // namespace pattern
